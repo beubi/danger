b74484ccd94087c5a843250a9f225aff5a0d5115
dmartinhorodrigues <diogo.rodrigues@beubi.com>
2017-01-20 17:27:34 +0000

    Revert changes

--- lib/danger/request_sources/bitbucket_cloud.rb
+++ lib/danger/request_sources/bitbucket_cloud.rb
@@ -53,12 +53,12 @@ module Danger
 
         # Next, we want to ensure that we have a version of the current branch at a known location
         puts scm.ensure_commitish_exists! base_commit
-        puts self.scm.exec "branch #{EnvironmentManager.danger_base_branch} #{base_commit}"
+        self.scm.exec "branch #{EnvironmentManager.danger_base_branch} #{base_commit}"
 
         # OK, so we want to ensure that we have a known head branch, this will always represent
         # the head of the PR ( e.g. the most recent commit that will be merged. )
         puts scm.ensure_commitish_exists! head_commit
-        puts self.scm.exec "branch #{EnvironmentManager.danger_head_branch} #{head_commit}"
+        self.scm.exec "branch #{EnvironmentManager.danger_head_branch} #{head_commit}"
       end
 
       def organisation
