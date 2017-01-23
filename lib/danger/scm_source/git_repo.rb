6a98f988072949f3c4bfa6b6d50e1da709e7aece
dmartinhorodrigues <diogo.rodrigues@beubi.com>
2017-01-23 10:18:33 +0000

    Fix output

--- lib/danger/scm_source/git_repo.rb
+++ lib/danger/scm_source/git_repo.rb
@@ -40,7 +40,7 @@ module Danger
     end
 
     def ensure_commitish_exists!(commitish)
-      git_shallow_fetch if commit_not_exists?(commitish)
+      git_shallow_fetch(repo, from, to) if commit_not_exists?(commitish)
 
       if commit_not_exists?(commitish)
         raise_if_we_cannot_find_the_commit(commitish)
@@ -76,7 +76,7 @@ module Danger
       possible_merge_base = possible_merge_base(repo, from, to)
       
       unless possible_merge_base
-        git_shallow_fetch
+        git_shallow_fetch(repo, from, to)
         possible_merge_base = possible_merge_base(repo, from, to)
       end
 
