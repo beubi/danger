a99563f03dddc32c43a401bc10435a057dc494d2
dmartinhorodrigues <diogo.rodrigues@beubi.com>
2017-01-23 14:24:27 +0000

    Test fetch

--- lib/danger/scm_source/git_repo.rb
+++ lib/danger/scm_source/git_repo.rb
@@ -57,8 +57,7 @@ module Danger
     private
 
     def git_shallow_fetch (repo, from, to)
-      puts "fetch --tags --progress https://${GIT_USERNAME}:${GIT_PASSWORD}@bitbucket.org/#{repo}.git refs/heads/#{from}:refs/remotes/origin/#{to}"
-      exec("fetch --tags --progress https://${GIT_USERNAME}:${GIT_PASSWORD}@bitbucket.org/" + repo + '.git +refs/heads/' + from + ':refs/remotes/origin/' + to) # before was fetch --unshallow
+      exec("fetch --tags --progress https://${DANGER_BITBUCKETCLOUD_USERNAME}:${DANGER_BITBUCKETCLOUD_PASSWORD}@bitbucket.org/#{repo}.git refs/heads/#{from}:refs/remotes/origin/#{to}") # before was fetch --unshallow
     end
 
     def default_env
